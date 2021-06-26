d21560ba499297537495abd7b3a68d70d589ae7f
keroro <keroroxx520@gmail.com>
2021-06-26 13:43:27 +0800

    feat: add RFC0223AfterSwitch

---
+++ src/case/rfc0223/after_switch.rs
@@ -0,0 +1,112 @@
+use crate::case::{Case, CaseOptions};
+use crate::node::{Node, NodeOptions};
+use crate::nodes::Nodes;
+use crate::util::{
+    since_from_absolute_epoch_number_with_fraction, since_from_relative_epoch_number_with_fraction,
+};
+use crate::CKB_FORK2021_BINARY;
+use ckb_types::{
+    core::{cell::CellMeta, EpochNumber, EpochNumberWithFraction, TransactionBuilder},
+    packed::{CellInput, CellOutput},
+    prelude::*,
+};
+
+const RFC0223_EPOCH_NUMBER: EpochNumber = 3;
+
+pub struct RFC0223AfterSwitch;
+
+impl Case for RFC0223AfterSwitch {
+    fn case_options(&self) -> CaseOptions {
+        CaseOptions {
+            make_all_nodes_connected: false,
+            make_all_nodes_synced: false,
+            make_all_nodes_connected_and_synced: false,
+            node_options: vec![NodeOptions {
+                node_name: "node-fork2021",
+                ckb_binary: CKB_FORK2021_BINARY.lock().clone(),
+                initial_database: "db/Epoch2V2TestData",
+                chain_spec: "spec/fork2021",
+                app_config: "config/fork2021",
+            }]
+            .into_iter()
+            .collect(),
+        }
+    }
+
+    fn run(&self, nodes: Nodes) {
+        let node_fork2021 = nodes.get_node("node-fork2021");
+
+        // Move the chain to height = rfc0221_switch
+        while !is_rfc0223_switched(node_fork2021) {
+            node_fork2021.mine(1);
+        }
+
+        let current_block_epoch = node_fork2021.get_tip_block().epoch();
+        let cells = node_fork2021.get_live_always_success_cells();
+        assert!(cells.len() >= 4);
+
+        let build_transaction = |since: u64, input: &CellMeta| {
+            TransactionBuilder::default()
+                .input(CellInput::new(input.out_point.clone(), since))
+                .output(
+                    CellOutput::new_builder()
+                        .lock(input.cell_output.lock())
+                        .type_(input.cell_output.type_())
+                        .capacity(input.capacity().pack())
+                        .build(),
+                )
+                .output_data(Default::default())
+                .cell_dep(node_fork2021.always_success_cell_dep())
+                .build()
+        };
+        let since_relative_epoch_number_with_fraction1 =
+            since_from_relative_epoch_number_with_fraction(EpochNumberWithFraction::new_unchecked(
+                0, 1801, 1800,
+            ));
+        let since_relative_epoch_number_with_fraction2 =
+            since_from_relative_epoch_number_with_fraction(EpochNumberWithFraction::new_unchecked(
+                0, 1800, 1800,
+            ));
+        let since_absolute_epoch_number_with_fraction1 =
+            since_from_absolute_epoch_number_with_fraction(EpochNumberWithFraction::new_unchecked(
+                current_block_epoch.number(),
+                2,
+                1,
+            ));
+        let since_absolute_epoch_number_with_fraction2 =
+            since_from_absolute_epoch_number_with_fraction(EpochNumberWithFraction::new_unchecked(
+                current_block_epoch.number(),
+                1,
+                1,
+            ));
+        let txs = vec![
+            build_transaction(since_relative_epoch_number_with_fraction1, &cells[0]),
+            build_transaction(since_relative_epoch_number_with_fraction2, &cells[1]),
+            build_transaction(since_absolute_epoch_number_with_fraction1, &cells[2]),
+            build_transaction(since_absolute_epoch_number_with_fraction2, &cells[3]),
+        ];
+
+        // Move forward to make sure our since values become valid
+        node_fork2021.mine(1800 + 10);
+
+        txs.iter().enumerate().for_each(|(i, tx)| {
+            let result = node_fork2021
+                .rpc_client()
+                .send_transaction_result(tx.pack().data().into());
+            assert!(
+                result
+                    .as_ref()
+                    .unwrap_err()
+                    .to_string()
+                    .contains("InvalidSince"),
+                "node_fork2021 should reject tx-{} according to rfc0223, but got: {:?}",
+                i,
+                result
+            );
+        });
+    }
+}
+
+fn is_rfc0223_switched(node: &Node) -> bool {
+    node.rpc_client().get_current_epoch().number.value() >= RFC0223_EPOCH_NUMBER
+}
--- src/case/rfc0223/mod.rs
+++ src/case/rfc0223/mod.rs
@@ -1,2 +1,5 @@
+pub(super) mod after_switch;
 pub(super) mod before_switch;
 mod util;
+
+// TODO add case to cover since EpochNumberWithFraction{ length=0, index=0 }
