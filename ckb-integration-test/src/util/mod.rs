470e0870e6275f67e881e93ba81169031001e6a1
keroro <keroroxx520@gmail.com>
2021-07-17 17:02:09 +0800

    fix(ckb-integration-test): correct typo

--- ckb-integration-test/src/util/mod.rs
+++ ckb-integration-test/src/util/mod.rs
@@ -1,8 +1,11 @@
-use ckb_types::core::{BlockNumber, EpochNumber};
 use ckb_testkit::Node;
+use ckb_types::core::{BlockNumber, EpochNumber};
 
 pub fn calc_epoch_start_number(node: &Node, epoch_number: EpochNumber) -> BlockNumber {
     assert!(node.consensus().permanent_difficulty_in_dummy);
-    let genesis_epoch= node.rpc_client().get_epoch_by_number(0).expect("genesis epoch should exist");
-    genesis_epoch.number.value() * epoch_number
+    let genesis_epoch = node
+        .rpc_client()
+        .get_epoch_by_number(0)
+        .expect("genesis epoch should exist");
+    genesis_epoch.length.value() * epoch_number
 }
