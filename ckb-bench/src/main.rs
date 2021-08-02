b8dc1b70fc16ba60194fe86f81d7569643042df7
keroro <keroroxx520@gmail.com>
2021-08-02 12:22:33 +0800

    fix(ckb-bench): connect nodes before executing commands

--- ckb-bench/src/main.rs
+++ ckb-bench/src/main.rs
@@ -567,6 +567,21 @@ fn init_logger() -> ckb_logger_service::LoggerInitGuard {
 }
 
 fn wait_for_nodes_sync(nodes: &Vec<Node>) {
+    for node_a in nodes.iter() {
+        for node_b in nodes.iter() {
+            if node_a.p2p_address() != node_b.p2p_address() && !node_a.is_p2p_connected(node_b)
+            {
+                if node_a.get_tip_block_number() < node_b.get_tip_block_number() {
+                    // An ibd node will not request GetHeaders from inbound peers.
+                    // https://github.com/nervosnetwork/ckb/blob/78fb281317aeaaa8b2621908cda79928ac697df4/sync/src/synchronizer/mod.rs#L543
+                    node_a.p2p_connect(node_b);
+                } else {
+                    node_b.p2p_connect(node_a);
+                }
+            }
+        }
+    }
+
     let max_tip_number = nodes
         .iter()
         .map(|node| node.get_tip_block_number())
